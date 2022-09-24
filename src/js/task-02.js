cc1f3c670cdf330cb49b10f79ba5bdae1c666a02
IvanKoshel98 <GitHubPassvord142>
2022-09-24 10:23:36 +0300

    task_02 complete

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const liForListFromArray = (arr) => 
+  arr.map((item) => {
+    const elLi = document.createElement("li");
+    elLi.innerText = item;
+    elLi.classList.add("item");
+    return elLi;
+  });
+ 
+const ingredientsList = document.querySelector("ul#ingredients");
+
+ingredientsList.append(...liForListFromArray(ingredients));
+
+
