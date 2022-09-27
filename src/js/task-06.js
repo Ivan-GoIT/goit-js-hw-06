c25d6a068a9e8ad18abf1fa4a9dc6579b4327072
IvanKoshel98 <GitHubPassvord142>
2022-09-27 19:29:38 +0300

    task5

---
+++ src/js/task-06.js
@@ -0,0 +1,11 @@
+const inputEl = document.querySelector("#validation-input");
+
+inputEl.addEventListener('blur', onInputValidFilling);
+
+function onInputValidFilling(event) {
+    
+    const { length } = inputEl.dataset;
+inputValue.length>=length?
+
+    console.log(length)
+}
